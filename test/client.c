d2c8c802bda2919bc69ea785cdc1d517d56f694c
root <root@client.shixinh-219494.htscookie-pg0.wisc.cloudlab.us>
2024-09-23 10:52:05 -0500

    client wget

--- test/client.c
+++ test/client.c
@@ -6,9 +6,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
 int main()
 {
+    char buf[8192];
+    char request[1024];
+    char request_template[]="GET / HTTP/1.1\r\nHost: %s\r\n\r\n";
+    int request_len=snprintf(request,1024,request_template,"10.19.0.3");
     int sock=socket(AF_INET,SOCK_STREAM,0);
     if(sock==-1)
     {
@@ -25,10 +30,18 @@ int main()
         printf("connect error\n");
         return -1;
     }
-    char buf[5000];
-    while(~recv(sock,buf,4096,0))
+    int total=0,last;
+    while(total<request_len)
     {
-        printf("%s\n",buf);
+	last=write(sock,request+total,request_len-total);
+	if(last==-1)
+	{
+	    perror("write");
+	    exit(-1);
+	}
+	total+=last;
     }
+    while((total=read(sock,buf,8192))>0);
+    if(total==-1) exit(-1);
     return 0;
 }
